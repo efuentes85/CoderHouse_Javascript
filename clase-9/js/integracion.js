14d919e6fc81f99edfcae0c6803c99d5342dffab
Eduardo Fuentes Velasco <efuentes85@gmail.com>
2017-01-04 21:58:11 -0300

    Workshop actualizado

--- clase-9/js/integracion.js
+++ clase-9/js/integracion.js
@@ -5,12 +5,69 @@ function mostrarToDoList() {
     var tareas = ToDoList.mostrarTareas() || [];
     ToDoList.setTareas(tareas);
 
+    // Creando menu para las tareas (Mostrar)
+    var menuTareas = document.createElement("div");
+    menuTareas.setAttribute("id","menu");
+
+    var btnMostrar = document.createElement("button");
+    btnMostrar.setAttribute("id","mostrar");
+    var txtBtn = document.createTextNode("Mostrar Tareas");
+    btnMostrar.appendChild(txtBtn);
+
+    var btnBorrar = document.createElement("button");
+    btnBorrar.setAttribute("id","borrarTodo");
+    var txtBtn2 = document.createTextNode("Borrar Todo");
+    btnBorrar.appendChild(txtBtn2);
+
+
+    menuTareas.appendChild(btnMostrar);
+    menuTareas.appendChild(btnBorrar);
+    document.body.appendChild(menuTareas);
+
+
+
+
     tareas.forEach(function (tarea) {
+        
+
+
         var elementoTarea = document.createElement("div");
+        elementoTarea.setAttribute("class","fondo");
+
+
+        // Botones
+        // Eliminar
+
+        var btnEliminar = document.createElement("button");
+        var textoEliminar = document.createTextNode("Eliminar");
+
+        btnEliminar.appendChild(textoEliminar);
+        btnEliminar.setAttribute("class","eliminar");
+        btnEliminar.addEventListener("click",function(){
+                ToDoList.eliminarTarea(this.id,"")
+        });
+
+
+    
+
+
+        elementoTarea.appendChild(btnEliminar);
+
+        // Cambiar estado
+
+        var btnEstado = document.createElement("button");
+        var textoEstado = document.createTextNode("Cambiar Estado");
+
+        btnEstado.appendChild(textoEstado);
+        btnEstado.setAttribute("class","estado");
+        elementoTarea.appendChild(btnEstado);
+
+
 
         // Titulo Tarea
         var tituloTarea = document.createElement("h1");
         var textoTarea = document.createTextNode(tarea.titulo);
+
 
         tituloTarea.appendChild(textoTarea);
         elementoTarea.appendChild(tituloTarea);
@@ -21,21 +78,28 @@ function mostrarToDoList() {
 
         descripcionTarea.appendChild(textoDescripcion);
         elementoTarea.appendChild(descripcionTarea); 
+
     
-
+        // Estado Tarea, checkbox
         var estadoTarea = document.createElement("input");
-        var atributoTarea = document.appendChild("type","checkbox");
+        estadoTarea.setAttribute("type","checkbox");
 
-        estadoTarea.appendChild(atributoTarea);
-        elementoTarea.appendChild(estadoTarea);
-        /*
-        checkbox.setAttribute("type","checkbox");
-        checkbox.setAttribute("class","estado");
-        checkbox.appendChild(checkbox);
-*/
-        
+
+
+          if (tarea.estado) {
+                 
+                elementoTarea.appendChild(estadoTarea);
+        } else {
+                estadoTarea.setAttribute("checked","checked");  
+                elementoTarea.appendChild(estadoTarea);
+           
+        }
+   
 
 
         document.body.appendChild(elementoTarea);
     });
+
+
+
 }
